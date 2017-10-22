213d035e31e544ccda07c848a662198d7d323ec5
git <kontakt@bartoszzawadka.pl>
2017-10-22 11:56:18 +0200

    Changed port for azure service

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  brServiceUrl: 'http://bill-registry-service.azurewebsites.net'
+  brServiceUrl: 'http://bill-registry-service.azurewebsites.net:8080'
 };
